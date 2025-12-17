bf4cf12f89f266434294fdb09267e44eb9409c9a
Harsh Guleria <guleriah59@gmail.com>
2025-12-18 01:25:48 +0530

    feat: initialize React TypeScript project with Vite
    - Add package.json with dependencies and scripts for development, build, linting, and preview.
    - Create Vite logo SVG file.
    - Add global styles in index.css and component styles in App.css.
    - Implement main application structure in App.tsx with ChaiCard, ChaiList, Counter, and OrderForm components.
    - Create ChaiCard component to display chai details.
    - Create ChaiList component to render a list of ChaiCard components.
    - Implement Counter component for tracking cups ordered.
    - Create OrderForm component for placing chai orders.
    - Define Chai type in types.ts for type safety.
    - Set up TypeScript configuration files for app and node environments.
    - Configure Vite for React project.

---
+++ reactts/src/App.tsx
@@ -0,0 +1,40 @@
+import "./App.css";
+import { ChaiCard } from "./components/ChaiCard.tsx";
+import ChaiList from "./components/ChaiList.tsx";
+import { Counter } from "./components/Counter.tsx";
+import { OrderForm } from "./components/OrderForm.tsx";
+
+import type { Chai } from "./types.ts";
+
+const menu: Chai[] = [
+  { id: 1, name: "Masala", price: 25 },
+  { id: 2, name: "Ginger", price: 50 },
+  { id: 3, name: "Lemon", price: 60 },
+];
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Vite + React</h1>
+        <ChaiCard name="Headphones" price={5000} />
+        <ChaiCard name="iPhone" price={80000} />
+      </div>
+      <div>
+        <Counter />
+      </div>
+      <div>
+        <ChaiList items={menu} />
+      </div>
+      <div>
+        <OrderForm
+          onSubmit={(order) => {
+            console.log("Placed:", order.name, order.cups);
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
